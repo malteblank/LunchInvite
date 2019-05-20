142b78f890f72419547ff0c8d79c7a66f3f2fc0c
Malte Blank <malte.blank@fs-students.de>
2019-05-20 18:07:23 +0200

    DATABASE + RECYCLERVIEW (UPDATED V3)

--- app/src/main/java/com/example/lunchinvite/InvitationsDBHelper.java
+++ app/src/main/java/com/example/lunchinvite/InvitationsDBHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class InvitationsDBHelper extends SQLiteOpenHelper
 {
     public static final String DATABASE_NAME = "invitations.db";
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
 
 
     public InvitationsDBHelper(Context context)
